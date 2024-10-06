3d54aaf93232f0bf4c0bc5bb891b079711193bb5
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-06 19:49:51 +0200

    Création du composant Router et implémentation dans App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import Header from "./components/Header";
+import AppRouter from './components/Router';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div>
-     <Header />
-     <Footer />
+      <Header />
+      <div>
+        <AppRouter />
+      </div>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Router.jsx
@@ -0,0 +1,24 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "../pages/HomePage.jsx";
+import SavedBouquetsPage from "../pages/SavedBouquetsPage.jsx";
+import ConnexionPage from "../pages/ConnexionPage.jsx";
+import ContactPage from "../pages/ContactPage.jsx";
+import ErrorPage from "../pages/ErrorPage.jsx";
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/bouquets-sauvegardés" element={<SavedBouquetsPage />} />
+        <Route path="/connexion" element={<ConnexionPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter;
