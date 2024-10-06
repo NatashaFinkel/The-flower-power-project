dc4da82d48a408ec22e5b144be91bc2634ebe9c4
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-06 09:09:48 +0200

    Remplacement du fichier main.jsx par index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App';
+import './index.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
