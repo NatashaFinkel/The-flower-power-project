fb4d24217b67142616f8085421919fc332a72760
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-06 19:28:46 +0200

    Création du composant Footer et implémentation dans App.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,47 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <div itemScope itemType="https://schema.org/Organization" className="footer">
+
+                <div className="footer-legal-infos-div">
+                    <p className="footer-title">Informations légales :</p><br></br>
+                    <p>Mentions légales</p><br></br>
+                    <p>CGV</p>
+                </div>
+
+                <div className="footer-shop-div">
+                    <p className="footer-title">Notre boutique : </p><br></br>
+                    <p className="brand-name" itemProp="legalName">Le jardin aux fleurs sauvages</p>
+                    <p itemProp="address" itemScope itemType="https://schema.org/PostalAddress">
+                        <span itemProp="streetAddress">1 rue du petit ruisseau</span><br></br>
+                        <span itemProp="postalCode">77600 </span>
+                        <span itemProp="addressLocality">Bussy-Saint-Georges</span>
+                    </p><br></br>
+
+                    <p>Ouverture du lundi au vendredi, de 10h à 19h.</p>
+
+                </div>
+
+                <div className="footer-contact-shop-div">
+                    <p className="footer-title">Contactez-nous : </p><br></br>
+
+                    <div className="footer-telephone-div">
+                        <i className="fa-solid fa-phone telephone"></i>
+                        <p itemProp="telephone">06 28 01 19 92</p><br></br>
+                    </div><br></br>
+
+                    <div className="footer-contact-form">
+                        <i className="fa-solid fa-envelope enveloppe"></i>
+                        <a href="/contact">Formulaire de contact</a>
+                    </div>
+                </div>
+
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
