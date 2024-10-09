eace2428cad7577edbd6a0f21cb24cf25719b308
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-09 18:24:15 +0200

    Ajout de NavBar dans le Router

--- src/components/Router.jsx
+++ src/components/Router.jsx
@@ -1,6 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./NavBar.jsx";
 import HomePage from "../pages/HomePage.jsx";
 import SavedBouquetsPage from "../pages/SavedBouquetsPage.jsx";
 import ConnexionPage from "../pages/ConnexionPage.jsx";
@@ -8,17 +9,18 @@ import ContactPage from "../pages/ContactPage.jsx";
 import ErrorPage from "../pages/ErrorPage.jsx";
 
 const AppRouter = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/bouquets-sauvegardés" element={<SavedBouquetsPage />} />
-        <Route path="/connexion" element={<ConnexionPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/bouquets-sauvegardés" element={<SavedBouquetsPage />} />
+                <Route path="/connexion" element={<ConnexionPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="*" element={<ErrorPage />} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRouter;
