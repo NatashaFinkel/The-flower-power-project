29b7fdc3df270b9875c3affc210386cb5748467d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-12 19:23:58 +0200

    Test : The app must render properly

---
+++ src/test/App.test.jsx
@@ -0,0 +1,8 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import App from "../App";
+import { test, expect } from "vitest";
+
+test('The app must render properly', () => {
+    expect((<App />))
+});
