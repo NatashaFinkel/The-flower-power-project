7b64fa832aad15a9e1caf91084d7daff274ce587
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-13 15:44:09 +0200

    Création du composant FormLabel

---
+++ src/components/FormLabel.jsx
@@ -0,0 +1,20 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import PropTypes from 'prop-types';
+import '../style/index.css';
+
+function FormLabel({ inputClassName, inputId, inputValue }) {
+    return (
+        <div className={inputClassName}>
+            <label htmlFor={inputId}>{inputValue}</label>
+        </div>
+    );
+}
+
+FormLabel.propTypes = {
+    inputClassName: PropTypes.string,
+    inputId: PropTypes.string,
+    inputValue: PropTypes.string,
+}
+
+export default FormLabel;
