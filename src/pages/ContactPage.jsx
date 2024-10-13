9a89e4cd5000c32684b48263a1e6ce5556ac8a7c
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-13 15:44:46 +0200

    Implémentation de FormInput et FormLabel dans ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,11 +1,57 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import FormLabel from "../components/FormLabel";
+import FormInput from "../components/FormInput";
 
 function ContactPage() {
     return (
-        <div>
-            <h1>Page contact</h1>
+        <div className="main-div">
+            <div className="title-div">
+                <h1>Contactez-nous !</h1>
+            </div>
+            <div>
+                <form>
+                    <FormLabel inputId="client-name" />
+
+                    <FormInput
+                        inputClassName="form-input"
+                        iconClassName="fa-solid fa-user contact-icon"
+                        inputType="text"
+                        inputId="client-name"
+                        inputPlaceholder="Nom et prénom"
+                    />
+
+                    <FormLabel inputClassName="flex-div" inputId="client-email" />
+
+                    <FormInput
+                        inputClassName="form-input"
+                        iconClassName="fa-solid fa-square-envelope contact-icon"
+                        inputType="email"
+                        inputId="client-email"
+                        inputPlaceholder="Adresse e-mail"
+                    />
+
+                    <div className="message-div">
+                        <FormLabel inputId="client-message" />
+                        <textarea
+                            id="client-message"
+                            name="client-message"
+                            rows="4"
+                            cols="50"
+                            placeholder="Votre message..."
+                        ></textarea>
+
+                        <FormInput
+                            inputType="submit"
+                            inputId="submit-message"
+                            inputValue="Envoyer"
+                            inputClassName="form-btn-div"
+                        />
+                    </div>
+                </form>
+            </div>
         </div>
-    )
+    );
 }
+
 export default ContactPage;
