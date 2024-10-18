eeb33b20ca85b112ea580d8d3db70c4979ede740
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-18 14:38:22 +0200

    Implémentation de Card et Modal dans HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import Card from '../components/Card';
+import Modal from "../components/Modal";
 
 function HomePage() {
     return (
-        <div>
-            <h1>Page accueil</h1>
+        <div className="main-div">
+            <div className="title-div">
+                <h1>Le jardin aux fleurs sauvages</h1>
+            </div>
+            <div className="gallery">
+                <Card />
+                <Modal />
+            </div>
         </div>
     )
 }
