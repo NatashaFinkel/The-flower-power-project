31ad2a1faac75670ea4ae80bcca5ea82be2bb0e5
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-22 22:39:09 +0200

    Ajout du texte d'introduction dans HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,6 +9,10 @@ function HomePage() {
             <div className="title-div">
                 <h1>Le jardin aux fleurs sauvages</h1>
             </div>
+            <div className="intro-div">
+                <p>Imaginez son sourire. Ce sourire qui éclaire son visage alors que la surprise succède à la joie.<br></br>Nul besoin d’attendre une occasion particulière pour voir ce sourire fleurir sur le visage de la personne que vous aimez !</p>
+                <p>Chacune de nos créations est unique et inspirée par la nature, pour offrir à vos proches un moment unique.</p>
+            </div>
             <div className="gallery">
                 <Card />
                 <Modal />
