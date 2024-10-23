b4e45434ab80497c7692407fee33686dfd01a107
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-23 21:53:59 +0200

    Implémentation de Redux : le store et shoppingListSlice.js

---
+++ src/redux/shoppingListSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    array: JSON.parse(localStorage.getItem("shoppingList")) || JSON.parse(sessionStorage.getItem("shoppingList")) || [],
+    isThisBouquetSaved: false,
+    isThereAnError: false,
+}
+
+const shoppingListSlice = createSlice({
+    name: 'addToShoppingList',
+    initialState,
+    reducers: {
+        addBouquetToShoppingList: (state, action) => {
+            const bouquetExists = state.array.some(
+                bouquet => bouquet.id === action.payload.id
+            );
+            if (!bouquetExists) {
+                state.array.push(action.payload);
+                state.isThisBouquetSaved = true;
+            } else {
+                state.array = state.array.filter(bouquet => bouquet.id !== action.payload.id);
+                state.isThisBouquetSaved = false;
+            }
+        }
+    },
+})
+export const { addBouquetToShoppingList } = shoppingListSlice.actions;
+export default shoppingListSlice.reducer;
