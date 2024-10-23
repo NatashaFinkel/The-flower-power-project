06058ee460d20d3316eac617252f0273f91fddd5
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-10-23 21:55:35 +0200

    Clean code et correction du fichier index.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-/* // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
 import App from './App';
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 import './style/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-); */
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
-import App from './App';
-import './style/index.css';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  React.createElement(StrictMode, null, 
+  React.createElement(Provider, { store: store },
     React.createElement(App, null)
   )
 );
