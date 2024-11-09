1e31570099c6ae58d5d3c6f08eb544741a2c3f0d
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-09 14:39:09 +0100

    Suppression de AppWrapper.test.jsx

--- src/test/AppWrapper.test.jsx
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
-import AppWrapper from '../components/AppWrapper';
-import { test, expect } from 'vitest';
-
-test('The app must render properly', () => {
-    expect((<AppWrapper />))
-});
