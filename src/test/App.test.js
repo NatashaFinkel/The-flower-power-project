340e2ebc3ee4608cf4aa1dea742fd25451dff1b5
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-09 14:40:16 +0100

    Test : The App must renders properly

---
+++ src/test/App.test.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Header from '../components/Header';
+
+describe('The App must renders properly', () => {
+    test('if the logo appears on the page, then the app does render properly', () => {
+        render(<Header />);
+        const logo = screen.getByTestId("logo-testid");
+        expect(logo).toBeInTheDocument();
+    });
+    
+    //TODO: faire un test pour chaque page ?
+});
