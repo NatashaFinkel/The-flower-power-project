f2c67c5a6297fb0cf74f4e1b3da4ebc028791e76
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-10 17:42:51 +0100

    Modification de l'intitulé du test de Modal.test.js par : Opens modal at click on the card

--- src/test/Card.test.js
+++ src/test/Card.test.js
@@ -5,7 +5,7 @@ import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import HomePage from '../pages/HomePage';
 
-test('Shows modal on click', () => {
+test('Opens modal at click on the card', () => {
     render(
         <Provider store={store}>
             <HomePage />
