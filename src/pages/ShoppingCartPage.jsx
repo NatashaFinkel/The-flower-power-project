56deae687980a299c30502ff3e58e5e17a27eb7f
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-12 07:54:54 +0100

    Implémentation de la fonction qui fait disparaître les containers de ShoppingCartPage s'il n'y a aucun bouquet dans le localStorage

--- src/pages/ShoppingCartPage.jsx
+++ src/pages/ShoppingCartPage.jsx
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
 import ShoppingItem from '../components/ShoppingItem';
 
 function ShoppingCartPage() {
@@ -7,13 +9,29 @@ function ShoppingCartPage() {
 
     //TODO: fonction "sous-total".
 
+    const array = useSelector(state => state.shoppingList.array);
+
+    useEffect(() => {
+        const shoppingItemContainer = document.getElementById("shopping-item-container");
+        const purchaseSummaryContainer = document.getElementById("purchase-summary-container");
+
+        if (array.length === 0) {
+            shoppingItemContainer.style.display = "none";
+            purchaseSummaryContainer.style.display = "none";
+
+        } else {
+            shoppingItemContainer.style.display = "flex";
+            purchaseSummaryContainer.style.display = "flex";
+        }
+    }, [array]);
+
     return (
         <div className="main-div">
             <div className="title-div">
                 <h1>Mon panier</h1>
             </div>
-            <div className="shopping-cart">
-                <div className="shopping-list" key="shopping-list" data-testid="shopping-item-container">
+            <div className="shopping-cart" id="shopping-cart">
+                <div className="shopping-list" key="shopping-list" id="shopping-item-container" data-testid="shopping-item-container-test-id">
                     <ShoppingItem />
                 </div>
                 <div className="purchase-summary-container" id="purchase-summary-container" data-testid="purchase-summary-container-test-id">
