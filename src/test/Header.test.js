84d00b13b693057a2102b11da15ff03811ed6850
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-20 08:35:39 +0100

    Correction orthographe

--- src/test/Header.test.js
+++ src/test/Header.test.js
@@ -4,7 +4,7 @@ import { describe, expect, test } from '@jest/globals';
 import { render, screen } from '@testing-library/react';
 import Header from '../components/Header';
 
-describe('The App must renders properly', () => {
+describe('The App must render properly', () => {
     test('if the logo appears on the page, then the app does render properly', () => {
         render(<Header />);
         const logo = screen.getByTestId("logo-test-id");
