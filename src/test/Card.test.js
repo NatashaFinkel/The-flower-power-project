4b268b858f55a5ef92514c800a3a51c3d01c667a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-20 21:50:33 +0100

    Test : The card contains an image and a title

--- src/test/Card.test.js
+++ src/test/Card.test.js
@@ -4,7 +4,33 @@ import {expect, test} from '@jest/globals';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
+import Card from '../components/Card';
 import HomePage from '../pages/HomePage';
+
+test('The card contains an image and a title', () => {
+    const bouquetTest = {
+        imageSrc: 'test-imageSrc-card-1.jpg',
+        imageAlt: 'test alt img card 1',
+        imageId: 'test-id-img-card-1',
+        imageTitle: 'test-title-card-1',
+        imageDescription: 'Test description img card 1',
+        imagePrice: 45,
+        imageDataTestid: 'data-test-id-img-card-1',
+    };
+
+    render(
+        <Card bouquet={bouquetTest} />
+    );
+
+    const testComponent = <Card bouquet={bouquetTest} />;
+    const testComponentProps = testComponent.props;
+
+    const testImageSrc = testComponentProps.bouquet.imageSrc;
+    const testImageTitle = testComponentProps.bouquet.imageTitle;
+
+    expect(testImageSrc).toBe('test-imageSrc-card-1.jpg');
+    expect(testImageTitle).toBe('test-title-card-1');
+});
 
 test('Opens modal at click on the card', () => {
     render(
