f6bd3270f922369d757a279fd146e540ef09c466
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-20 21:51:05 +0100

    Modification de l'indentation du code

--- src/test/Card.test.js
+++ src/test/Card.test.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
-import {expect, test} from '@jest/globals';
+import { test, expect } from '@jest/globals';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
@@ -44,4 +44,4 @@ test('Opens modal at click on the card', () => {
     fireEvent.click(cards[0]);
 
     expect(modal).toBeVisible();
-});
+}); 
