84ea6b13e4d160190f7421ca7bddd04a0594cd83
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-24 14:42:40 +0100

    Fix bug : clic possible sur la card entière au lieu de l'image uniquement

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ function Card() {
         let modalPrice = document.getElementById("modal-price");
 
         modal.style.display = "flex";
-        modalImgSrc.src = selectedBouquet.src;
+        modalImgSrc.src = selectedBouquet.getAttribute("src");
         modalImgSrc.alt = selectedBouquet.alt;
         modalTitle.textContent = selectedBouquet.title;
         modalDescription.textContent = selectedBouquet.getAttribute("description");
@@ -31,9 +31,14 @@ function Card() {
             {
                 flowers && flowers.map(flower => {
                     return (
-                        <div key={flower.index} id={flower.id} className="card" onClick={showModal} data-testid={`card-test-id-${flower.index}`}>
-                            <Image imageSrc={flower.imgSrc} imageAlt={flower.imgAlt} imageId={`card-${flower.index}`} imageTitle={flower.imgTitle} imageDescription={flower.imgDescription} imagePrice={flower.price} imageDataTestid={`card-img-${flower.index}-test-id`} />
-                            <p className="card-title">{flower.imgTitle}</p>
+                        // eslint-disable-next-line react/no-unknown-property
+                        <div key={flower.index} id={flower.id} className="card" src={flower.imgSrc} alt={flower.imgAlt} title={flower.imgTitle} description={flower.imgDescription} price={flower.price} onClick={showModal} data-testid={`card-test-id-${flower.index}`}>
+
+                            <Image imageId={`card-${flower.index}-img`} imageSrc={flower.imgSrc} imageAlt={flower.imgAlt} imageTitle={flower.imgTitle} imageDescription={flower.imgDescription} imagePrice={flower.price} imageDataTestid={`card-img-${flower.index}-test-id`} imageClassName="card-img" />
+
+                            {/* eslint-disable-next-line react/no-unknown-property */}
+                            <p className="card-title" src={flower.imgSrc} alt={flower.imgAlt} title={flower.imgTitle} description={flower.imgDescription} price={flower.price}>{flower.imgTitle}</p>
+
                         </div>
                     )
                 })
