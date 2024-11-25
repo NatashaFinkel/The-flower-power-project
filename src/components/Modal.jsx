3d606189eb226c54b0ce3c2dbe87aa4d2f510b1a
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-25 14:57:15 +0100

    Clean code

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -44,7 +44,7 @@ function Modal({ mImgSrc, mImgAlt, mTitle, mDescription, mPrice }) {
         let modalPriceTxtContent = modalPrice.textContent;
 
         const bouquet = { id: modalContentId, title: modalContentTitleTxtContent, imgSrc: modalContentImgSrc, imgAlt: modalContentAlt, description: modalDescriptionTxtContent, price: modalPriceTxtContent };
-        console.log(bouquet);
+
         dispatch(addToShoppingList(bouquet));
     };
 
