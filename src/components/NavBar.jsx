1f5f4941acbf7b88c5ed0a58d148e88b1600f622
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-30 20:03:53 +0100

    Implémentation de AppNavLink dans NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,34 +1,56 @@
-// eslint-disable-next-line no-unused-vars
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import AppNavLink from './AppNavLink';
 
 function NavBar() {
+  //  le rendu est conditionel :
+  //   si l'écran est un téléphone (largeur d'écran plus grande que 425px), les liens de navigation seront des icônes; sinon ce sera du texte.
+
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const breakpoint = 425;
+  React.useEffect(() => {
+    const handleResizeWindow = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResizeWindow);
+    return () => {
+      window.removeEventListener("resize", handleResizeWindow);
+    };
+  }, []);
+
+  if (width > breakpoint) {
+    return (
+      <div className="nav-bar-div">
+        <nav>
+          <ul>
+            <li>
+              <AppNavLink navLinkDirection="/" navLinkActiveclassname="active" navLinkTxt="Accueil" />
+            </li>
+            <li>
+              <AppNavLink navLinkDirection="/bouquets-sauvegardés" navLinkActiveclassname="active" navLinkTxt="Mon panier" />
+            </li>
+            <li>
+              <AppNavLink navLinkDirection="/contact" navLinkActiveclassname="active" navLinkTxt="Contact" />
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
   return (
     <div className="nav-bar-div">
       <nav>
         <ul>
           <li>
-            <NavLink to="/" activeclassname="active">
-              <i className="fa-solid fa-house nav-bar-icon icon-display-none"></i>
-              <p>Accueil</p>
-            </NavLink>
+            <AppNavLink navLinkDirection="/" navLinkActiveclassname="active" navLinkIconClassName="fa-solid fa-house nav-bar-icon icon-display-none" />
           </li>
           <li>
-            <NavLink to="/bouquets-sauvegardés" activeclassname="active">
-              <i className="fa-solid fa-cart-shopping nav-bar-icon icon-display-none"></i>
-              <p>Mon panier</p>
-            </NavLink>
+            <AppNavLink navLinkDirection="/bouquets-sauvegardés" navLinkActiveclassname="active" navLinkIconClassName="fa-solid fa-cart-shopping nav-bar-icon icon-display-none" />
           </li>
           <li>
-            <NavLink to="/contact" activeclassname="active">
-              <i className="fa-regular fa-comment-dots nav-bar-icon icon-display-none"></i>
-              <p>Contact</p>
-            </NavLink>
+            <AppNavLink navLinkDirection="/contact" navLinkActiveclassname="active" navLinkIconClassName="fa-regular fa-comment-dots nav-bar-icon icon-display-none" />
           </li>
         </ul>
       </nav>
     </div>
-  );
+  )
 }
 
 export default NavBar;
