583170b42dfd167601b5ef91bccba1e348033b3f
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-11-30 20:03:12 +0100

    Création du composant AppNavLink.jsx

---
+++ src/components/AppNavLink.jsx
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { NavLink } from 'react-router-dom';
+
+function AppNavLink({ navLinkDirection, navLinkClassName, navLinkActiveclassname, navLinkIconClassName, navLinkTxt }) {
+    return (
+        <div>
+            <NavLink to={navLinkDirection} className={navLinkClassName} activeclassname={navLinkActiveclassname}>{navLinkTxt}<i className={navLinkIconClassName}></i>
+            </NavLink>
+        </div>
+    )
+}
+
+AppNavLink.propTypes = {
+    navLinkDirection: PropTypes.string,
+    navLinkClassName: PropTypes.string,
+    navLinkActiveclassname: PropTypes.string,
+    navLinkIconClassName: PropTypes.string,
+    navLinkTxt: PropTypes.string,
+}
+export default AppNavLink;
