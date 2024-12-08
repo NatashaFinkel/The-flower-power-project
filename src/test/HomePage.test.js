d2f09ec87a9a2b9f48880648f33b6a320b8e4b82
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-08 19:28:42 +0100

    Rectification de l'importation de flowers.json dans HomePage.test.js

--- src/test/HomePage.test.js
+++ src/test/HomePage.test.js
@@ -5,7 +5,8 @@ import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { store } from '../redux/store';
 import HomePage from '../pages/HomePage';
-import flowers from '../../flowers.json';
+/* import flowers from '../../flowers.json'; */
+import flowers from '../json/flowers.json';
 
 describe('Gallery items', () => {
     test('Each item from the flowers.json is rendered on the bouquets gallery', () => {
