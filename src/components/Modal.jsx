0707d3b288c3ea86c85e5c3be0b4335b1e266d76
Natasha Finkelsztajn <nfinkelsztajn@gmail.com>
2024-12-18 20:37:53 +0100

    Ajout message de confirmation d'ajout bouquet dans panier et implémentation de closeModal après ce message

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -3,6 +3,8 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { addToShoppingList } from '../redux/shoppingListSlice';
 import Image from './Image';
+import PopUp from './PopUp';
+import addedToCartConfirmationPopUpImg from '/assets/added-to-cart-confirmation-popUp-img.png';
 import PropTypes from 'prop-types';
 
 function Modal({ mImgSrc, mImgAlt, mTitle, mDescription, mPrice }) {
@@ -52,7 +54,7 @@ function Modal({ mImgSrc, mImgAlt, mTitle, mDescription, mPrice }) {
     }, []);
 
     const handleAddBouquet = () => {
-        const { modalContent, modalTitle, modalImg, modalDescription, modalPrice } = getModalElements();
+        const { modalContent, modalTitle, modalImg, modalDescription, modalPrice, modal } = getModalElements();
         let modalContentId = modalContent.id;
         let modalContentTitleTxtContent = modalTitle.textContent;
         let modalContentImgSrc = modalImg.src;
@@ -60,9 +62,26 @@ function Modal({ mImgSrc, mImgAlt, mTitle, mDescription, mPrice }) {
         let modalDescriptionTxtContent = modalDescription.textContent;
         let modalPriceTxtContent = modalPrice.textContent;
 
+        function AddedToCartConfirmationMessageAndCloseModal() {
+            setTimeout(() => {
+                modalContent.id = ""; modalImg.src = ""; modalImg.alt = "modal : image alt par défaut"; modalTitle.textContent = "modal : titre par défaut"; modalDescription.textContent = ""; modalPrice.textContent = ""; modal.style.display = "none";
+            }, 700);
+
+            const popup = document.getElementById("popup");
+            const overlay = document.getElementById("overlay");
+            popup.style.display = 'block';
+            overlay.style.display = 'block';
+
+            setTimeout(() => {
+                popup.style.display = 'none';
+                overlay.style.display = 'none';
+            }, 700);
+        };
+
         const bouquet = { id: modalContentId, title: modalContentTitleTxtContent, imgSrc: modalContentImgSrc, imgAlt: modalContentAlt, description: modalDescriptionTxtContent, price: modalPriceTxtContent };
 
         dispatch(addToShoppingList(bouquet));
+        AddedToCartConfirmationMessageAndCloseModal();
     };
 
     useEffect(() => {
@@ -94,6 +113,7 @@ function Modal({ mImgSrc, mImgAlt, mTitle, mDescription, mPrice }) {
                     <button onClick={handleAddBouquet} className="add-bouquet-btn" id="add-bouquet-btn" data-testid="add-bouquet-btn-test-id">Ajouter au panier</button>
                 </div>
             </div>
+            <PopUp popUpImgSrc={addedToCartConfirmationPopUpImg} popUpImgAlt="logo du popUp de confirmation d'ajout au panier" popUpImgClassName="added-to-cart-confirmation-message-popup-img" popUpMessage="Votre bouquet a été ajouté !" />
         </div>
     )
 }
